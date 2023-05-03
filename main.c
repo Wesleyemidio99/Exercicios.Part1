81dbfb8e257571cd77df853a2df0821413c1678f
wesley <wesleyemidio99@hotmail.com>
2023-05-03 00:51:01 -0300

    Atualizacao do exercicio 1,2, e finalizacao do exercicio 4.

--- main.c
+++ main.c
@@ -3,17 +3,18 @@
 void exercicioUm(){
     int a,b,c;
 
+    system("cls");
     printf("Digite 3 numeros inteiros:\n");
     scanf("%d",&a);
     scanf("%d",&b);
-    scanf("$d",&c);
+    scanf("%d",&c);
 
-    printf("O primeiro numero foi:%d\nO segundo foi %d\n O terceiro numero foi %d\n",a,b,c);
+    printf("O primeiro numero foi:%d\nO segundo numero foi %d\nO terceiro numero foi %d\n",a,b,c);
 }
 
 
-void exercicioDois(){
-    int a = 20,b = 3,adicao,subtracao,produto;
+void exercicioDois(int a,int b){
+    int adicao,subtracao,produto;
     float divisao;
 
     adicao = a+b;
@@ -21,6 +22,7 @@ void exercicioDois(){
     produto = a*b;
     divisao = a/b;
 
+    system("cls");
     printf("A Soma de %d e %d = %d\n",a,b,adicao);
     printf("A Subtracao de %d e %d = %d\n",a,b,subtracao);
     printf("A divisao de %d e %d = %f.5\n",a,b,divisao);
@@ -32,7 +34,8 @@ void exercicioTres(){
     int f;
     float celsius;
 
-    printf("\nDigite um determinado grau em Fahrenheit (32-212):\n");
+    system("cls");
+    printf("Digite um determinado grau em Fahrenheit (32-212):\n");
     scanf("%d",&f);
 
     celsius = 5.0/9.0 * (f-32.0);
@@ -41,12 +44,31 @@ void exercicioTres(){
 
 }
 
+void exercicioQuatro(){
+    int s,so,v,t;
+
+    system("cls");
+    printf("Digite a distancia inical:");
+    scanf("%d",&so);
+    printf("Digite a velocidade constante:");
+    scanf("%d",&v);
+    printf("Digite o tempo:");
+    scanf("%d",&t);
+
+    s = so+(v*t);
+
+    printf("O espaco percorrido = %d",s);
+
+}
+
 void main() {
     int resposta;
 
     printf("Exercicios 1 ate 50 (part1)\n\n");
-    printf("1 - Escrever um programa que permita ao usuario digitar tres números inteiros e os imprima:\n");
+    printf("1 - Escrever um programa que permita ao usuario digitar tres numeros inteiros e os imprima:\n");
     printf("2 - Escrever um programa que execute e imprima o resultado das operacoes de adicao, subtracao, divisao e produto entre dois numero decimais inicializados com 20 e 3.\n");
+    printf("3 - Escrever um programa que permita ao usuario digitar um valor referente a um determinado grau Fahrenheit (32-212). Calcule a conversao deste grau em Celsius.\n");
+    printf("4 - Escrever um programa que permite calcular o espaco percorrido por uma velocidade constante. \n");
     printf("Digite qual execercicio deseja:");
     scanf("%d",&resposta);
 
@@ -57,11 +79,15 @@ void main() {
             break;
 
         case 2:
-            exercicioDois();
+            exercicioDois(20,3);
             break;
 
         case 3 :
             exercicioTres();
+            break;
+
+        case 4:
+            exercicioQuatro();
             break;
     }
 
